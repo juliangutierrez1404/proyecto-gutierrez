6e46cda7fd826efdea2e777bd7ce0ec6662ea2d0
Julian Gutierrez <gutierrez1404@outlook.com>
2024-08-22 17:54:08 -0300

    Pre entrega 2

--- js/index.js
+++ js/index.js
@@ -17,50 +17,19 @@ imagen.addEventListener('mouseout', function(){
     imagen.classList.remove("flip")
 })
 
+let cambiarWidth = document.getElementById('cambiarWidth');
+
+cambiarWidth.addEventListener('click', function(){
+    cambiarWidth.classList.add("achicar");
+});
+cambiarWidth.addEventListener('mouseup', function(){
+    cambiarWidth.classList.remove('achicar');
+});
 /* Animacion del backgroundcolor/aboutMe */
 
-let cambioColor = document.getElementById("cambioColor");
-let colores = cambioColor.classList;
+// const COLORES = ['red', 'blue', 'yellow'];
 
-        if (colores == "gris") {
-            cambioColor.addEventListener('mousedown', function(){
-                colores.remove("gris");
-                colores.add("blanco");
-            });
-        } else if (colores == "blanco") {
-            cambioColor.addEventListener('mousedown', function(){
-                colores.remove("blanco");
-                colores.add("beige");
-            });
-        } else if (colores == "beige") {
-            cambioColor.addEventListener('mousedown', function(){
-                colores.remove("beige");
-                colores.add("gris");
-            });
-        }
-
-
-
-
-
-// const COLOR_1 = "blanco";
-// const COLOR_2 = "beige";
-
-// cambioColor.addEventListener('click', function(){
-//     if (cambioColor.classList = "gris"){
-//     cambioColor.addEventListener('click', function(){
-//         cambioColor.classList.remove("gris");
-//         cambioColor.classList.add(COLOR_1);
-//     })
-// }else if (cambioColor.classList = COLOR_1){
-//     cambioColor.addEventListener('click', function(){
-//         cambioColor.classList.remove(COLOR_1);
-//         cambioColor.classList.add(COLOR_2);
-//     })
-// }else if (cambioColor.classList = COLOR_2){
-//     cambioColor.addEventListener('click', function(){
-//         cambioColor.classList.remove(COLOR_2);
-//         cambioColor.classList.add("gris");
-//     })
-// }
-// })
+// document.getElementById("cambioColor").addEventListener('click', function(){
+//     this.style.backgroundColor = COLORES[indexActual];
+//     let indexActual = (indexActual + 1) % COLORES.length;
+// });
