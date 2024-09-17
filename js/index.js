3c8bcf0789024b5c7fde7bb057b2e0cd9f16a527
Julian Gutierrez <gutierrez1404@outlook.com>
2024-09-17 20:45:37 -0300

    Actualizacion de header

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
-let titulo = document.getElementById('mainTitle');
+// let titulo = document.getElementById('mainTitle');
 
-titulo.addEventListener('mousedown', function(){
-    titulo.innerText = "Fabiana Lazzo";
-});
+// titulo.addEventListener('mousedown', function(){
+//     titulo.innerText = "Fabiana Lazzo";
+// });
 
-titulo.addEventListener('mouseup', function(){
-    titulo.innerText = "Quien soy";
-});
+// titulo.addEventListener('mouseup', function(){
+//     titulo.innerText = "Quien soy";
+// });
 
-let imagen = document.getElementById('profileImg');
+// let imagen = document.getElementById('profileImg');
 
-imagen.addEventListener('mouseover', function(){
-    imagen.classList.add("flip");
-});
-imagen.addEventListener('mouseout', function(){
-    imagen.classList.remove("flip")
-})
+// imagen.addEventListener('mouseover', function(){
+//     imagen.classList.add("flip");
+// });
+// imagen.addEventListener('mouseout', function(){
+//     imagen.classList.remove("flip")
+// })
 
-let cambiarWidth = document.getElementById('cambiarWidth');
+// let cambiarWidth = document.getElementById('cambiarWidth');
 
-cambiarWidth.addEventListener('click', function(){
-    cambiarWidth.classList.add("achicar");
-});
-cambiarWidth.addEventListener('mouseup', function(){
-    cambiarWidth.classList.remove('achicar');
-});
+// cambiarWidth.addEventListener('click', function(){
+//     cambiarWidth.classList.add("achicar");
+// });
+// cambiarWidth.addEventListener('mouseup', function(){
+//     cambiarWidth.classList.remove('achicar');
+// });
 /* Animacion del backgroundcolor/aboutMe */
 
 // const COLORES = ['red', 'blue', 'yellow'];
